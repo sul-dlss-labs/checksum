766d2e83c789dc67bdda9f10b799f9b7dda720a1
Justin Coyne <jcoyne@justincoyne.com>
2018-04-19 09:58:32 -0700

    Add the StoredFile wrapper

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -5,15 +5,11 @@ class FilesController < ApplicationController
   # Upload a new file by doing:
   #   curl -Ffile=@README.md localhost:3000/files
   #
-  # On success it will return a 204 status with a Location header
-  # pointing at the download path.
+  # On success it will return a 201 status with the file's
+  # key.
   def create
     attachable = params[:file]
-    blob = ActiveStorage::Blob.create_after_upload!(
-      io: attachable.open,
-      filename: attachable.original_filename,
-      content_type: attachable.content_type
-    )
-    head :no_content, location: blob
+    file = StoredFile.create_from_upload(attachable)
+    render json: { id: file.key }, status: :created
   end
 end
---
+++ app/models/stored_file.rb
@@ -0,0 +1,26 @@
+# A wrapper around ActiveStorage::Blob that expects the local disk adapter
+class StoredFile
+  def self.create_from_upload(attachable)
+    blob = ActiveStorage::Blob.create_after_upload!(
+      io: attachable.open,
+      filename: attachable.original_filename,
+      content_type: attachable.content_type
+    )
+    new.with_blob(blob)
+  end
+
+  def self.find(key)
+    new.with_blob(ActiveStorage::Blob.find_by(key: key))
+  end
+
+  def with_blob(blob)
+    @blob = blob
+    self
+  end
+
+  def path
+    ActiveStorage::Blob.service.send(:path_for, key)
+  end
+
+  delegate :key, to: :@blob
+end
